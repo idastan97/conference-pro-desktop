2d3b299e3515077498d325d44bba06d7ed578946
Dastan <idastan97@gmail.com>
2019-11-21 01:11:20 +0600

    initial

---
+++ main.py
@@ -0,0 +1,51 @@
+import serial
+import queue
+from flask import Flask, request, jsonify
+import time
+import _thread
+PORT = "/dev/ttyACM0"
+SLEEP_TIME = 2
+app = Flask(__name__)
+qu = queue.Queue()
+
+
+@app.route('/put', methods=['GET'])
+def add_point():
+    data = request.args
+    print(data)
+    x = data.get('x')
+    y = data.get('y')
+    print(x, y)
+    qu.put((int(x), int(y)))
+    return 'ok'
+
+
+def send_command(ser, com):
+    com += "\r\n"
+    ser.write(com.encode())
+    ser.flush()
+    # time.sleep(1)
+
+
+def main():
+    print("segg")
+    ser = serial.Serial(PORT, 115200)
+    if ser.is_open:
+        time.sleep(3)
+        com = u"G0 F500"
+        send_command(ser, com)
+        time.sleep(3)
+        while True:
+            try:
+                x, y = qu.get()
+                print("get", x, y)
+                com = f"G01 X{x} Y{y}"
+                send_command(ser, com)
+            finally:
+                time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    _thread.start_new_thread(app.run, (), {'host': '0.0.0.0'})
+    # app.run(host='0.0.0.0')
+    main()
